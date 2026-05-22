005c47ac3b45722e0513afe2739e6acea484b006
SarahSpeedy <dubeyharshit0605@gmail.com>
2026-05-22 07:46:32 +0530

    Added Favourite section+Dynamic local storage

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ const API_URL="https://www.omdbapi.com/?i=tt3896198&apikey=84b49b42";
 const searchInput=document.getElementById('searchInput');
 const searchBtn=document.getElementById("searchBtn");
 const movieResults=document.getElementById('movieResults');
-
+const favorites=document.getElementById('favorites');
+const favoriteMovies=JSON.parse(localStorage.getItem('favorite'))||[];
+document.addEventListener('DOMContentLoaded',displayFavorites);
 async function fetchMovies(query){
     try{
     const response=await fetch(`${API_URL}&s=${query}`);
@@ -28,9 +30,34 @@ function displayMovies(movies) {
             <img src="${movie.Poster}" alt="${movie.Title}">
             <h3>${movie.Title}</h3>
             <p>${movie.Year}</p>
+            <button onclick="addtofavorites('${movie.imdbID}','${movie.Title.replace("'","\\"+"'")}','${movie.Poster}','${movie.Year}');">Add to favorites</button>
+
         `;
 
         movieResults.appendChild(movieCard);
+    });
+}
+function addtofavorites(id ,title,poster,year) {
+    if(favoriteMovies.some(movie=>movie.id===id)){
+        return;}
+    favoriteMovies.push({id,title,poster,year});
+    localStorage.setItem('favorite',JSON.stringify(favoriteMovies));
+    displayFavorites();
+}
+function displayFavorites() {
+     favorites.innerHTML='';
+    favoriteMovies.forEach((movie) => {
+        const movieCard = document.createElement('div');
+
+        movieCard.innerHTML = `
+            <img src="${movie.poster}" alt="${movie.title}">
+            <h3>${movie.title}</h3>
+            <p>${movie.year}</p>
+          <button>Remove</button>
+
+        `;
+
+        favorites.appendChild(movieCard);
     });
 }
 
